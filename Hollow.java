e66539c597fa45d3f4056e1286357532e449f99a
Shaik Saniya Anjum <shaiksaniyaanjum956@gmail.com>
2026-02-28 13:32:59 +0530

    Add files via upload

---
+++ Hollow.java
@@ -0,0 +1,16 @@
+public class Hollow {
+    public static void main(String[] args) {
+        int rows = 5;
+        for(int i = 1; i <= rows; i++) {
+            for(int j = 1; j <= i; j++) {
+                if(i == 1 || i == rows || j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
